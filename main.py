7d43daada3e0f42b99696b280370b2ea072c9c4c
lev <Dmslmrk@icloud.com>
2022-12-22 10:00:55 +0300

    donedone

--- main.py
+++ main.py
@@ -1,19 +1,3 @@
 import controller as cr
 
 cr.main()
-
-
-# with open('book.csv', 'r', encoding='utf-8') as book:
-#             buffer_export2 = []
-#             search_element = input('Введите данные для фильтрации: ')
-#             for line in book:
-#                 buffer_export2.append(line)
-#             print(buffer_export2)
-#             print(search_element)
-#             for i in buffer_export2:
-#                 print(i)
-#                 if(search_element in i):
-#                     print("найдено")
-                    
-#                 else:
-#                     print("Не найдено")
